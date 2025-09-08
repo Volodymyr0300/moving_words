55f2b7f0819d1c8732f9a6c0ed52c9531290ba98
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-07 22:29:16 -0500

    commit

---
+++ target/debug/deps/moving_words-7a8298ba6b778660.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/moving_words/moving_words/target/debug/deps/libmoving_words-7a8298ba6b778660.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/moving_words/moving_words/target/debug/deps/moving_words-7a8298ba6b778660.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/moving_words-8fc0d73271ccbfda.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/moving_words/moving_words/target/debug/deps/libmoving_words-8fc0d73271ccbfda.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/moving_words/moving_words/target/debug/deps/moving_words-8fc0d73271ccbfda.d: src/main.rs
+
+src/main.rs:
