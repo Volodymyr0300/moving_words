cf5f1f669919455f483149b8c6532ab01cbf0cfb
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-08 21:03:24 -0500

    fn for check a vowel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use unicode_segmentation::unicode_segmentation;
+use unicode_normalization::UnicodeNormalization;
+
+fn is_vowel(grapheme:&str) -> bool {
+    if let Some(base) = grapheme.ntd().next() () {
+        let base_level = base.to_lowercase().next().unwrap_or(base);
+        matches!(base_level, 'a' | 'e' | 'i' | 'o' | 'u')
+    } else {
+        false
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
