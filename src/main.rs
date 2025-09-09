9478239adad0cdd4448c429d9861822b770611bf
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-08 21:41:12 -0500

    fn to pig latin word, append word, move first to the end, some producing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use unicode_segmentation::unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
 use unicode_normalization::UnicodeNormalization;
 
 
 fn is_vowel(grapheme:&str) -> bool {
-    if let Some(base) = grapheme.ntd().next() () {
+    if let Some(base) = grapheme.nfd().next() {
         let base_level = base.to_lowercase().next().unwrap_or(base);
         matches!(base_level, 'a' | 'e' | 'i' | 'o' | 'u')
     } else {
@@ -19,8 +19,31 @@ fn to_pig_latin_word(word: &str) -> String {
 
     let first = graphemes[0];
 
-    
+    if let Some(base) = first.nfd().next() {
+        if !base.is_alphabetic() {
+            return word.to_string();
+        } else {
+            return word.to_string();
+        }
+
+        if is_vowel(first) {
+            return format!("{}-hay", word);
+        }
+
+        let rest = if graphemes.len() > 1 {
+            graphemes[1..].concat()
+        } else {
+            String::new()
+        };
+
+        if rest.is_empty() {
+            format!("{}-ay", first)
+        } else {
+            format!("{}-{}ay", rest, first)
+        }
+    }
 }
+
 
 fn main() {
     println!("Hello, world!");
